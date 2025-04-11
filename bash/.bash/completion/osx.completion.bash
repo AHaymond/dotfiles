9ca54858932418b07df5f69b640b85afa97be4ba
Adam Haymond <coastgrd@gmail.com>
2025-04-11 13:48:30 -0600

    updates from mac

--- bash/.bash/completion/osx.completion.bash
+++ bash/.bash/completion/osx.completion.bash
@@ -1,12 +1,14 @@
 if [[ "$OSTYPE" =~ "darwin" ]]; then
-  if [ -f `brew --prefix`/etc/bash_completion ]; then
-    . `brew --prefix`/etc/bash_completion
+  if [ `command -v 'brew'` ]; then
+    if [ -f `brew --prefix`/etc/bash_completion ]; then
+      . `brew --prefix`/etc/bash_completion
+    fi
+
+    # Add tab completion for `defaults read|write NSGlobalDomain`
+    # You could just use `-g` instead, but I like being explicit
+    complete -W "NSGlobalDomain" defaults
+
+    # Add `killall` tab completion for common apps
+    complete -o "nospace" -W "Finder Dock Mail Safari iTunes iCal Address\ Book SystemUIServer" killall
   fi
-
-  # Add tab completion for `defaults read|write NSGlobalDomain`
-  # You could just use `-g` instead, but I like being explicit
-  complete -W "NSGlobalDomain" defaults
-
-  # Add `killall` tab completion for common apps
-  complete -o "nospace" -W "Finder Dock Mail Safari iTunes iCal Address\ Book SystemUIServer" killall
 fi
